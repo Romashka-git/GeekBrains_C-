61c10fee0666c2bbf9dded8b05ee498fa5d49a64
Журавлев Роман Александрович <zhuravlev.ra@esphere.local>
2023-06-22 12:35:32 +0300

    add summ

--- Lection/Lesson_1/Sum/Program.cs
+++ Lection/Lesson_1/Sum/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int result = (a + b);
+Console.WriteLine(result);
