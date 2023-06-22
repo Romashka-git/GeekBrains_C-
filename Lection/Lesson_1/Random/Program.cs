fefec8f3236e4253aa98d5ccc161a3f93338b561
Журавлев Роман Александрович <zhuravlev.ra@esphere.local>
2023-06-22 20:56:35 +0300

    add programm User

--- Lection/Lesson_1/Random/Program.cs
+++ Lection/Lesson_1/Random/Program.cs
@@ -1,4 +1,6 @@
-﻿int a = new Random().Next(1, 22);
-int b = new Random().Next(1, 33);
-int result = (a + b);
+﻿double a = new Random().Next(1, 22);
+Console.WriteLine(a);
+double b = new Random().Next(1, 33);
+Console.WriteLine(b);
+double result = (a / b);
 Console.WriteLine(result);
---
+++ Lection/Lesson_1/User/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите ваше имя");
+string name = Console.ReadLine();
+if (name.ToLower() == "заюш")
+    {
+        Console.WriteLine("Ты ж мой хороший");
+    }
+else
+{
+    Console.WriteLine("Hello");
+}
