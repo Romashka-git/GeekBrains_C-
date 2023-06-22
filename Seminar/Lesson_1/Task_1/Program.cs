e0ac01445f9d366f4b61f3f2baa66ef85290573e
Журавлев Роман Александрович <zhuravlev.ra@esphere.local>
2023-06-22 21:37:28 +0300

    add square

---
+++ Seminar/Lesson_1/Task_1/Program.cs
@@ -0,0 +1,6 @@
+﻿// Написать программу, что принимает число на ввод и выводит квадрат этого числа
+
+Console.Write("Введите число");
+int number = int.Parse(Console.ReadLine());
+int result = number * number;
+Console.WriteLine("Квадрат числа " + number + " равен " + result);
