729d5a01196c5996103cedb49f9457d1f44129e1
Журавлев Роман Александрович <zhuravlev.ra@esphere.local>
2023-06-24 02:06:41 +0300

    add max of 3

---
+++ Seminar/HM_1/Task_2/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход 
+// три числа и выдает максимальное из этих чисел.
+
+int max;
+Console.WriteLine("Введите колличество вводимых чисел");
+int n = int.Parse(Console.ReadLine());
+int[] nums = new int [n];
+
+for (int i = 0; i < n; i++)
+{
+    Console.WriteLine("Введите число");
+    int a = int.Parse(Console.ReadLine());
+    nums[i] = a;
+}
+max = nums[0];
+for (int i = 0; i < n; i++)
+{
+    if (nums[i] > max)
+    {
+        max = nums[i];
+    }
+    else 
+    {
+        max = max;
+    }
+} 
+Console.WriteLine($"Максимальное число {max}");
