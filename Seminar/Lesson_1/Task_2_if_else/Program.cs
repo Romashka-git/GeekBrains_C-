6f118e429eea62e2a1775d9e51b8d9c6df169182
Журавлев Роман Александрович <zhuravlev.ra@esphere.local>
2023-06-25 17:59:51 +0300

    Случайное изменение

---
+++ Seminar/Lesson_1/Task_2_if_else/Program.cs
@@ -0,0 +1,14 @@
+﻿// Написать программу, что принимает число на ввод и выводит квадрат этого числа
+
+Console.Write("Введите число ");
+int number = int.Parse(Console.ReadLine());
+
+if (number <= 10)
+{
+    Console.WriteLine("Надо было больше 10");
+}
+else
+{
+    int result = number * number;
+    Console.WriteLine($"Квадрат числа {number} равен {result}");   
+}
