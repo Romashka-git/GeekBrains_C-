92fa0f5df8bed93a36b557acc2a8ab9f78e5382a
Журавлев Роман Александрович <zhuravlev.ra@esphere.local>
2023-06-29 00:03:25 +0300

    Добавили Задачку из лекции

---
+++ Lection/Lesson_2/Task_1_array/Program.cs
@@ -0,0 +1,27 @@
+﻿int [] array = new int [10];
+
+
+void FillArray (int [] collection)
+{
+    int n = collection.Length;
+    for (int i = 0; i < n; i++)
+    {
+        collection[i] = new Random().Next(1, 10);
+    }
+}
+
+void PrintArray (int [] collection_2)
+{
+    int n_2 = collection_2.Length;
+    for (int i = 0; i < n_2; i++)
+    {
+        System.Console.WriteLine($"{i + 1} элемент массива - {collection_2[i]}");
+    }
+}
+
+FillArray (array);
+PrintArray (array);
+int [] a = array;
+System.Console.WriteLine(a);
+System.Console.WriteLine(array);
+System.Console.WriteLine(a[0]);
---
+++ Seminar/Lesson_2/Task_1/Program.cs
@@ -0,0 +1,12 @@
+﻿int a = 1567;
+int b;
+string c = "";
+while (a != 0)
+{
+    b = a;
+    b = b % 10;
+    a = a / 10;
+    c = ($"{c}{b}");
+}
+int d = int.Parse(c);
+Console.WriteLine(d);
