469a13c2b55cee1165bbd68acfa5395d6d8897e6
Журавлев Роман Александрович <zhuravlev.ra@esphere.local>
2023-07-13 23:16:55 +0300

    Добавили ДЗ по Семинару 6

--- Seminar/HM_6/Task_1/Program.cs
+++ Seminar/HM_6/Task_1/Program.cs
@@ -1,1 +1,28 @@
 ﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользовател
+
+
+string N = "q"; // символ для выхода из цикла
+string usN = String.Empty; // оператор выхода из цикла
+int numUs = 0;
+List<int> nums = new List<int>();
+int count = 0;
+int count0 = 0;
+
+System.Console.WriteLine("Введите числа, а я посчитаю, сколько числе больше 0");
+
+while (N != usN)
+{
+    System.Console.WriteLine($"Введите {count + 1} число: ");
+    numUs = int.Parse(Console.ReadLine());
+    if ( numUs > 0) count0++;
+    nums.Add(numUs);
+    count++;
+    System.Console.WriteLine("Введите 'q', если хотите выйти или любой другой символ, чтобы продолжить");
+    usN = Console.ReadLine().ToLower();
+}
+
+string result = string.Join(" ", nums.ToArray());
+System.Console.WriteLine(result);
+System.Console.WriteLine($"Количество чисел больше нуля: {count0}");
+
+
--- Seminar/HM_6/Task_2/Program.cs
+++ Seminar/HM_6/Task_2/Program.cs
@@ -2,3 +2,39 @@
 // точку пересечения двух прямых, заданных уравнениями 
 // y = k1 * x + b1, y = k2 * x + b2; 
 // значения b1, k1, b2 и k2 задаются пользователем.
+
+List<double> Func (double b1, double k1, double b2, double k2)
+{
+    List <double> f = new List<double>();
+    double x = Math.Round((-b2 + b1)/(-k1 + k2), 2);
+    double y = Math.Round((k2 * x + b2), 2);
+    f.Add(x);
+    f.Add(y);
+    return f;
+}
+System.Console.WriteLine("Введите b1: ");
+double b1 = int.Parse(Console.ReadLine());
+
+System.Console.WriteLine("Введите k1: ");
+double k1 = int.Parse(Console.ReadLine());
+
+System.Console.WriteLine("Введите b2: ");
+double b2 = int.Parse(Console.ReadLine());
+
+System.Console.WriteLine("Введите k2: ");
+double k2 = int.Parse(Console.ReadLine());
+
+System.Console.Clear();
+
+double [] result = Func(b1, k1, b2, k2).ToArray();
+
+string print = string.Join(" ", result);
+System.Console.WriteLine(print);
+
+
+
+// for (int i = 0; i < result.Length; i++)
+// {
+//     System.Console.WriteLine($"{result[i]}");
+// }
+
