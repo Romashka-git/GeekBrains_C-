b4b79d5c9ce88ca7165d59502ab8efeea827508a
Журавлев Роман Александрович <zhuravlev.ra@esphere.local>
2023-07-27 04:57:01 +0300

    Добавили ДЗ по 9-ому Семинару

---
+++ Seminar/HM_9/Task_4.1/Program.cs
@@ -0,0 +1,43 @@
+﻿// Даны два положительных целых числа, представленных в виде строк. Необходимо получить сумму чисел, которые представлены этими строками. Причем полученная сумма должна быть преобразована в строку.
+// Для решения этой задачи запрещено преобразовывать входные строки сразу напрямую в целое число при помощи встроенной функции. Необходимо сделать это самостоятельно.
+
+// Пример 1:
+// Входные значения: num1 = "11", num2 = "123"
+// Результат: "134"
+
+// Пример 2:
+// Входные значения: num1="456", num2 = "77"
+// Результат: "533"
+
+// Пример 3:
+// Входные значения: num1 = "0", num2 = "0"
+// Результат: "0"
+
+string num1 = "11"; 
+string num2 = "123";
+
+int ConvertNum (string str)
+{
+    int countLength = str.Length - 1;
+    int result = 0;
+    for (int i = 0; i < str.Length; i++)
+    {
+        if (char.IsNumber(str[i]))
+            {
+                result = result + (Convert.ToInt32(str[i].ToString()) * Convert.ToInt32(Math.Pow(10, countLength)));
+                countLength--;
+            }
+    }
+    return result;
+}
+
+string Summ (int a, int b)
+{
+    return Convert.ToString(a + b);
+}
+
+int number1 = ConvertNum(num1);
+int number2 = ConvertNum(num2);
+
+string resultSumm = Summ(number1, number2);
+System.Console.WriteLine(resultSumm);
---
+++ Seminar/Lesson_9/Task_1/Program.cs
@@ -0,0 +1,14 @@
+﻿string a = "11";
+List<string> b = new List<string>();
+
+string c = string.Empty;
+for (int i = 0; i < a.Length; i++)
+{
+    c = a[i].ToString();   
+    b.Add(c);
+}
+
+foreach (var ig in b)
+{
+    Console.WriteLine(ig);
+}
